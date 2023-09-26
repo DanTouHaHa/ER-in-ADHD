59fa2365ef965444a71a458f8bf243608ea2f6ed
DanTouHaHa <74862506+DanTouHaHa@users.noreply.github.com>
2023-09-26 21:59:59 +0800

    Update 8 ADHD-200 clinical dataset validation.R

--- 8 ADHD-200 clinical dataset validation.R
+++ 8 ADHD-200 clinical dataset validation.R
@@ -91,7 +91,78 @@ result.to_excel("Inattentive 100*5 result.xlsx", index=False)
 
 
 
-#####机器学习-----LDA模型
+#随机选1000次
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+from scipy.stats import pearsonr
+import random
+
+##### 每次抽取3个脑区，随机抽取1000次，得到1000个r，每个r是单次抽取进行五折交叉验证的平均r值
+
+def fold5(x, y):
+    # 将数据拆成5份
+    kf = KFold(n_splits=5)
+    x_train_list=[]
+    y_train_list=[]
+    x_test_list=[]
+    y_test_list=[]
+    for i, (train_index, test_index) in enumerate(kf.split(x)):
+        print("第{}折".format(i))
+        # print(train_index)
+        # print(test_index)
+        # print(x[test_index])
+        x_train_list.append(x[train_index])
+        y_train_list.append(y[train_index])
+        x_test_list.append(x[test_index])
+        y_test_list.append(y[test_index])
+    return x_train_list, y_train_list, x_test_list, y_test_list
+
+data=pd.read_excel("ADHD_200 patients prediction 2-2.xlsx")
+select_cols=data.columns[-70:]
+all_X=data[select_cols]
+
+indexList=[]
+for i in range(1000): # 这里为了简单写的是随机抽取1000次
+    temp=[68, 69] # gender和age肯定在
+    for j in range(3):
+        temp.append(random.randint(0, 67))
+    indexList.append(temp)
+
+xlsx_data=pd.DataFrame()
+for item in indexList:
+    # print(item)
+    X=all_X.values[:, item]
+    print(X.shape)
+    Y=data["Hyper"].values # 选择Y变量
+
+    X_train_list, Y_train_list, X_test_list, Y_test_list=fold5(X, Y)
+
+    
+    all_data_dict=dict()
+    # 计算平均显著性，平均相关性
+    corr_list=[]
+    pvalue_list=[]
+    for x_train, y_train, x_test, y_test in zip(X_train_list, Y_train_list, X_test_list, Y_test_list):
+        all_data_dict["name"]=",".join(all_X.keys()[x_index] for x_index in item)
+        reg = LinearRegression() # 线性回归
+        reg.fit(x_train, y_train)
+        print("训练系数:\n", reg.coef_) # 输出系数
+        # print(reg.predict(x_test).shape)
+        # print(y_test.shape)
+        corr, pvalue=pearsonr(reg.predict(x_test), y_test)
+        print("相关性系数:", corr)
+        print("显著性:", pvalue)
+        corr_list.append(corr)
+        pvalue_list.append(pvalue)
+    all_data_dict["平均相关性"]=sum(corr_list)/len(corr_list)
+    all_data_dict["平均显著性"]=sum(pvalue_list)/len(pvalue_list)
+    xlsx_data=xlsx_data.append(all_data_dict, ignore_index=True)
+
+xlsx_data.to_excel("ADHDHyper结果.xlsx", index=False)
+
+
+#####机器学习
 # install.packages(c('caret', 'skimr', 'RANN', 'randomForest', 'fastAdaboost', 'gbm', 'xgboost', 'caretEnsemble', 'C50', 'earth'))
 setwd("E:/Fudan_Luoqiang_ADHD_Project/20220923_ABCD数据库信息/20221111_DataAnalysis/机器学习分类/Persistent or remitted/机器学习")
 
@@ -131,7 +202,7 @@ auc_area<-slot(auc,"y.values")[[1]]; #auc_area<-auc@"y.values"[[1]];
 auc_area<-round(auc_area,4)
 
 
-#####机器学习-----GBM模型
+#####机器学习
 # install.packages(c('caret', 'skimr', 'RANN', 'randomForest', 'fastAdaboost', 'gbm', 'xgboost', 'caretEnsemble', 'C50', 'earth'))
 setwd("E:/Fudan_Luoqiang_ADHD_Project/20220923_ABCD数据库信息/20221111_DataAnalysis/机器学习分类/Persistent or remitted/机器学习")
 
